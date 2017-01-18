3d21fafce30b6056aef9c807fd0ca4adad7bce47
Matthew Davies <daviesgeek@icloud.com>
2017-01-17 23:29:37 -0800

    Got the basics of an application working

---
+++ application/app.js
@@ -0,0 +1,30 @@
+var app = new Application()
+
+app.registerSingleton('Logger', function () {
+  var Logger = function () {
+    this.log = function (value) {
+      console.log(value)
+    }
+  }
+
+  return new Logger()
+})
+
+app.registerController('AppController', function (Logger, data) {
+  Logger.log('test!!!')
+  console.log(data)
+})
+
+app.registerRoute('home', {
+  url: '/',
+  controller: 'AppController',
+  resolve: {
+    data: function () {
+      return Q.Promise(function(resolve) {
+        resolve({data: 'whooot!'});
+      });
+    }
+  }
+});
+
+app.boot()
---
+++ src/application.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var Application = function () {
+
+  this._controllers = {}
+
+  this.registerController = function (name, func) {
+    this._controllers[name] = func
+  }
+
+  this.registerSingleton = function (name, func) {
+    DI.bindSingleton(name, func)
+  }
+
+  this.registerRoute = function (name, route) {
+    Router.defineRoute(name, route)
+  }
+
+  this.boot = function () {
+    Router.onRouteChange(this.onRouteChange.bind(this))
+    Router.startListening()
+
+    // console.log('booting application')
+  }
+
+  this.onRouteChange = function (prevRoute, route) {
+
+    var resolvedData = {}
+
+    if(route.resolve) {
+      var promises = [Q.Promise(function (resolve) {
+        resolve()
+      })]
+
+      for(var item in route.resolve) {
+        var resolve = DI.call(route.resolve[item])
+
+        // If it returns a promise
+        if('then' in resolve) {
+          promises.push(resolve)
+          resolve.then(function(response) {
+            resolvedData[item] = response
+          })
+        } else {
+          resolvedData[item] = resolve
+        }
+
+      }
+
+      Q.all(promises).then(function () {
+        DI.call(this._getController(route.controller), resolvedData)
+      }.bind(this))
+    }
+
+  }
+
+  this._getController = function(name) {
+    if (!(name in this._controllers)) {
+      throw new ReferenceError('controller "' + name + '" is not defined')
+    }
+
+    return this._controllers[name]
+  }
+
+}
