e5476a6f92c5cdff496f9aadd2375752f69bc14f
Matthew Davies <daviesgeek@icloud.com>
2017-01-17 18:53:10 -0800

    Comments!

--- di.js
+++ di.js
@@ -4,14 +4,31 @@ DI._singletons          = {};
 DI._singletonInstances  = {};
 DI._factories           = {};
 
+/**
+ * Bind a factory value
+ * @param  {string} value   the value name
+ * @param  {function} factory the factory function
+ * @return {void}
+ */
 DI.bindFactory = function (value, factory) {
   this._factories[value] = DI.call(factory)
 }
 
+/**
+ * Bind a singleton value
+ * @param  {string} value     the value name
+ * @param  {Object} singleton the singleton
+ * @return {void}
+ */
 DI.bindSingleton = function(value, singleton) {
   this._singletons[value] = singleton
 }
 
+/**
+ * Resolves a value
+ * @param  {string} value
+ * @return {function}
+ */
 DI.resolve = function (value) {
 
   if(!(value in this._singletons) && !(value in this._singletonInstances) && !(value in this._factories)) {
@@ -27,6 +44,11 @@ DI.resolve = function (value) {
   return this._singletonInstances[value]
 }
 
+/**
+ * Calls a given function, injecting the dependencies as needed!
+ * @param  {function} func
+ * @return {mixed} the function result
+ */
 DI.call = function (func) {
 
   var funcArgs = this._parseArgs(func)
@@ -40,6 +62,11 @@ DI.call = function (func) {
   return func.apply(func, resolved)
 }
 
+/**
+ * Parses the argument names from a given function
+ * @param  {function} func
+ * @return {array} the function's arguments
+ */
 DI._parseArgs = function(func) {
   var argMatch = func.toString().match(/function\s.*?\(([^)]*)\)/)
   if(argMatch && argMatch.length > 0) {
@@ -56,4 +83,5 @@ DI._parseArgs = function(func) {
 
 }
 
+// Make it globally available
 window.DI = DI
